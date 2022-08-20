7ab1c5d148c5d8370699840eccd11a931ea28d90
Tory Chen <toryc333@gmail.com>
2022-08-19 22:21:26 -0400

    Basic Component Folder Structure

---
+++ src/components/DisplayAnimal/DisplayAnimal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const DisplayAnimal = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default DisplayAnimal;
---
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ResultsPage = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default ResultsPage;
