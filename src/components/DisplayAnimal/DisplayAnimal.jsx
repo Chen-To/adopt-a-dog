7c0e72240a61461966f029fa2d1004e50bdb0cfc
Tory Chen <toryc333@gmail.com>
2022-08-20 11:36:42 -0400

    Webpack config updated and wrote fetch request handler

--- src/components/DisplayAnimal/DisplayAnimal.jsx
+++ src/components/DisplayAnimal/DisplayAnimal.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export const DisplayAnimal = () => {
+    const [animalImage, setAnimalImage] = useState();
+    
+    useEffect(() => {
+        const getAnimalImage = async () => {
+            const resp = await fetch("https://dog.ceo/api/breeds/image/random");
+            const info = resp.json();
+            if (info.status === "success") {
+                setAnimalImage(info.message);
+            }
+        }
+        getAnimalImage();
+    });
+
     return (
         <>
         </>
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
     ],
     devServer: {
         port: 3000,
+        historyApiFallback: true,
         devMiddleware: {
             writeToDisk: true
         }
