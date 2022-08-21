53770f94b51367767187baf1fdb95c9d49123ff6
BobbyLiang10 <bobbyliang@hotmail.com>
2022-08-20 21:00:49 -0400

    Update HomePage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -93,6 +93,7 @@ export const HomePage = (props) => {
               }}
             >
         <Container maxWidth="sm">
+        <Box sx={{ p: 3 }} />
 <Typography variant="h4" align="center" color="text.secondary" paragraph>
 What you need to do.
 </Typography>
@@ -111,21 +112,12 @@ Click the red X button if the dog is not for you.
 At the end, we will determine which breeds you love the most!
 </Typography>
 <Typography variant="h5" align="center" color="text.secondary" paragraph>
-  How many dogs would you like to see?
+  Note: Please make a selection of at least 10 dogs.
   </Typography>
-  <Stack
-                sx={{ pt: 4 }}
-                direction="row"
-                spacing={2}
-                justifyContent="center"
-                >
-                <Button onClick = {(e) => handleScroll()} variant="contained">Enter Number of Dogs</Button>
-                </Stack>
 </Container>
         </div>
 
-
-        {/* APP ITSEF GOES HERE */}
+        {/* APP ITSELF GOES HERE */}
 
         <Box sx={{ p: 3 }} />
 
@@ -134,45 +126,6 @@ At the end, we will determine which breeds you love the most!
                 <DisplayAnimal dispatch = {props.dispatch}/>
             </Container>
         </Box>
-
-
-        <div 
-            style={{
-                width: "100%",
-                height: 800,
-              }}
-            >
-        <Container maxWidth="sm">
-<Typography variant="h4" align="center" color="text.secondary" paragraph>
-What you need to do.
-</Typography>
-  <Box sx={{ p: 2 }}/>
-  <Box sx={{ borderRadius: 1, borderColor:'secondary.main'}}/>
-<Typography variant="h5" align="center" color="text.secondary" paragraph>
-Randomly generated dogs will appear below.
-</Typography>
-<Typography variant="h5" align="center" color="text.secondary" paragraph>
-Click the green paw button if you love what you see.
-</Typography>
-<Typography variant="h5" align="center" color="text.secondary" paragraph>
-Click the red X button if the dog is not for you.
-</Typography>
-<Typography variant="h5" align="center" color="text.secondary" paragraph>
-At the end, we will determine which breeds you love the most!
-</Typography>
-<Typography variant="h5" align="center" color="text.secondary" paragraph>
-  How many dogs would you like to see?
-  </Typography>
-  <Stack
-                sx={{ pt: 4 }}
-                direction="row"
-                spacing={2}
-                justifyContent="center"
-                >
-                <Button onClick = {(e) => handleScroll()} variant="contained">Enter Number of Dogs</Button>
-                </Stack>
-</Container>
-        </div>
 
         </main>
       {/* Footer */}
