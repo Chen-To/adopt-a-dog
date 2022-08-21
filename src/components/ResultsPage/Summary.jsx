3e82c7386dab498dbfc6b360cb84c259ad37827a
saheeen-jey <saheen_j@ymail.com>
2022-08-21 01:35:18 -0400

    final summary

--- src/components/ResultsPage/Summary.jsx
+++ src/components/ResultsPage/Summary.jsx
@@ -7,17 +7,18 @@ export const Summary = (props) => {
 
         return (
             <Box sx ={{width: "100%", height: "100%"}} justify="space-between" justifyContent="center">
+                <Typography text-align="center" >Here are all the photos and breeds that you liked!</Typography>
                 <ImageList sx={{
                     gridAutoFlow: "row",
-                    gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr)) !important",
+                    gridTemplateColumns: "repeat(auto-fill,minmax(124px,1fr)) !important",
                 }} columns = {3} rowHeight={1} justify="space-between">
                 
                 {props.dogsSelected.map(items=> (
-                    <Card sx={{height: 350}} key={items.photo}>
+                    <Card sx={{height: 240}} key={items.photo}>
                         <CardContent>
                         <ImageListItem key={items.photo}>
                             <img src={items.photo} alt={items.breed} loading ='lazy'></img>
-                            <Typography>{items.breed}</Typography>
+                            <Typography text-align="center">{items.breed}</Typography>
                         </ImageListItem>
                         </CardContent >
                         
